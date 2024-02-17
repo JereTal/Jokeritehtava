a72bce5329b7cbc47cdd8dffed76693eebc7c523
Jere Talala <JereTal>
2024-02-17 17:10:00 +0200

    Valmis versio2

---
+++ JS/Functions.js
@@ -0,0 +1,26 @@
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    let button = document.querySelector('button');
+    let taulu = document.querySelector('#taulu');
+    button.addEventListener('click',luorivi);
+
+    let rivimaara = 0;
+    let maaraid = document.getElementById('tr');
+
+    function luorivi(){
+        let rivi = document.createElement('tr');
+        for (let i=0;i<7;i++){
+            let numero = Math.floor(Math.random()* 10);
+            let ruutu = document.createElement('td');
+            ruutu.textContent = numero;
+            rivi.appendChild(ruutu);
+        }
+        taulu.appendChild(rivi);
+        rivilaskuri();
+    }
+    function rivilaskuri() {
+        rivimaara++;
+        maaraid.textContent = "Valmiita rivejä " + rivimaara;
+    }
+});
